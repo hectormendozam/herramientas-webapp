b8987295753905e26aadf90dbcfc36ea0cfaa963
hectormendozam <hector.mendozam@alumno.buap.mx>
2023-11-25 23:50:18 -0600

    Comment en materias service

--- src/app/services/materias.service.ts
+++ src/app/services/materias.service.ts
@@ -87,14 +87,14 @@ export class MateriasService {
     return this.http.post<any>(`${environment.url_api}/materias/`,data, httpOptions);
   }
 
-    //Registrar materia
+    //Lista de materias
     public obtenerListaMaterias (): Observable <any>{
       var token = this.facadeService.getSessionToken();
       var headers = new HttpHeaders({ 'Content-Type': 'application/json' , 'Authorization': 'Bearer '+token});
       return this.http.get<any>(`${environment.url_api}/lista-materias/`, {headers:headers});
     }
 
-      //Obtener una sola materia dependiendo su ID
+    //Obtener una sola materia dependiendo su ID
     public getMateriaByID(idMateria: Number){
       return this.http.get<any>(`${environment.url_api}/materias/?id=${idMateria}`,httpOptions); 
     }
