564b03a1252570069929b3bd857fb5bb5876ba66
alejandroamadorlag <134448417+alejandroamadorlag@users.noreply.github.com>
2024-04-15 09:39:57 -0600

    Avance 4
    Front-end del registro terminado.

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -23,18 +23,15 @@ export class UsuariosService {
 
   public esquemaUser(){
     return {
-      'matricula': '',
       'first_name': '',
       'last_name': '',
-      'email': '',
+      'id_trabajador': '',
+      'hora_inicio': '',
+      'hora_final': '',
+      'puesto': '',
+      'username': '',
       'password': '',
       'confirmar_password': '',
-      'fecha_nacimiento': '',
-      'curp': '',
-      'rfc': '',
-      'edad': '',
-      'telefono': '',
-      'ocupacion': '',
     }
   }
 
@@ -42,10 +39,6 @@ export class UsuariosService {
   public validarUsuario(data: any, editar: boolean){    
     console.log("Validando user... ", data);
     let error: any = [];
-
-    if(!this.validatorService.required(data["matricula"])){
-      error["matricula"] = this.errorService.required;
-    }
 
     if(!this.validatorService.required(data["first_name"])){
       error["first_name"] = this.errorService.required;
@@ -55,12 +48,24 @@ export class UsuariosService {
       error["last_name"] = this.errorService.required;
     }
 
-    if(!this.validatorService.required(data["email"])){
-      error["email"] = this.errorService.required;
-    }else if(!this.validatorService.max(data["email"], 40)){
-      error["email"] = this.errorService.max(40);
-    }else if (!this.validatorService.email(data['email'])) {
-      error['email'] = this.errorService.email;
+    if(!this.validatorService.required(data["id_trabajador"])){
+      error["id_trabajador"] = this.errorService.required;
+    }
+
+    if(!this.validatorService.required(data["hora_inicio"])){
+      error["hora_inicio"] = this.errorService.required;
+    }
+
+    if(!this.validatorService.required(data["hora_final"])){
+      error["hora_final"] = this.errorService.required;
+    }
+
+    if(!this.validatorService.required(data["puesto"])){
+      error["puesto"] = this.errorService.required;
+    }
+
+    if(!this.validatorService.required(data["username"])){
+      error["username"] = this.errorService.required;
     }
 
     if(!editar){
@@ -71,44 +76,6 @@ export class UsuariosService {
       if(!this.validatorService.required(data["confirmar_password"])){
         error["confirmar_password"] = this.errorService.required;
       }
-    }
-
-    if(!this.validatorService.required(data["fecha_nacimiento"])){
-      error["fecha_nacimiento"] = this.errorService.required;
-    }
-
-    if(!this.validatorService.required(data["curp"])){
-      error["curp"] = this.errorService.required;
-    }else if(!this.validatorService.min(data["curp"], 18)){
-      error["curp"] = this.errorService.min(18);
-      alert("La longitud de caracteres de la CURP es menor, deben ser 18");
-    }else if(!this.validatorService.max(data["curp"], 18)){
-      error["curp"] = this.errorService.max(18);
-      alert("La longitud de caracteres de la CURP es mayor, deben ser 18");
-    }
-
-    if(!this.validatorService.required(data["rfc"])){
-      error["rfc"] = this.errorService.required;
-    }else if(!this.validatorService.min(data["rfc"], 12)){
-      error["rfc"] = this.errorService.min(12);
-      alert("La longitud de caracteres deL RFC es menor, deben ser 12");
-    }else if(!this.validatorService.max(data["rfc"], 13)){
-      error["rfc"] = this.errorService.max(13);
-      alert("La longitud de caracteres deL RFC es mayor, deben ser 13");
-    }
-
-    if(!this.validatorService.required(data["edad"])){
-      error["edad"] = this.errorService.required;
-    }else if(!this.validatorService.numeric(data["edad"])){
-      alert("El formato es solo números");
-    }
-
-    if(!this.validatorService.required(data["telefono"])){
-      error["telefono"] = this.errorService.required;
-    }
-
-    if(!this.validatorService.required(data["ocupacion"])){
-      error["ocupacion"] = this.errorService.required;
     }
 
     return error;
